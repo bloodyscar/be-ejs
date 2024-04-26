960f91df66c440ee49926254c8e152134c2063db
Adit <adithernowo@gmail.com>
2024-04-26 14:26:06 +0700

    chnage db

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ const mysql = require('mysql2');
 
 // create the connection to database
 const connection = mysql.createPool({
-    host: "localhost",
+    // host: "localhost",
+    host: "103.206.246.227",
     user: "root",
     password: "",
     database: "db_karyawan",
